e5f88dbaf407a5354435fedc11355e21f094eae7
AlbanMarku <albanmarku@outlook.com>
2023-07-27 11:57:58 +0100

    Input context in khar.

--- views/Kharacter.js
+++ views/Kharacter.js
@@ -1,11 +1,17 @@
 import { View, Text, Image } from 'react-native';
+import { useContext } from 'react';
+import { InputContext } from '../Context';
+
 export default function Kharacter({ route, navigation }) {
   const { name, img } = route.params;
-  console.log(JSON.stringify(img));
+
+  const { input } = useContext(InputContext);
+
   return (
     <View style={{ flex: 1, justifyContent: 'center' }}>
       <Text>I am character page {JSON.stringify(name)}</Text>
       <Image style={{ height: 100 }} source={{ uri: img }} />
+      <Text>input is {input}</Text>
     </View>
   );
 }
