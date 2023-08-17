7600ac3151c81eefe232b5b022f904e6bfec5c3d
AlbanMarku <albanmarku@outlook.com>
2023-08-17 17:27:27 +0100

    Basic styling.

--- components/OverviewComp.js
+++ components/OverviewComp.js
@@ -1,40 +1,80 @@
 import { Text, View, StyleSheet } from 'react-native';
 
 export default function OverviewComp({ overviewObj }) {
-  console.log(overviewObj);
+  console.log(overviewObj.strengths);
 
   const RenderStrengths = () => {
     return (
-      <View style={{ backgroundColor: 'red', flex: 1 }}>
-        {overviewObj.strengths.map((item, index) => {
-          return <Text key={index}>{item}</Text>;
-        })}
+      <View>
+        <View style={{ alignItems: 'center', padding: 15 }}>
+          <Text style={{ color: 'white', fontFamily: 'mk11', fontSize: 22 }}>Strengths</Text>
+        </View>
+        <View>
+          {overviewObj.strengths.map((item, index) => {
+            return (
+              <View key={index}>
+                <Text style={[styles.arrayText, index % 2 === 0 ? styles.even : styles.odd]}>
+                  {item}
+                </Text>
+              </View>
+            );
+          })}
+        </View>
       </View>
     );
   };
 
   const RenderWeaknesses = () => {
     return (
-      <View style={{ backgroundColor: 'red', flex: 1 }}>
-        {overviewObj.weaknesses.map((item, index) => {
-          return <Text key={index}>{item}</Text>;
-        })}
+      <View>
+        <View style={{ alignItems: 'center', padding: 15 }}>
+          <Text style={{ color: 'white', fontFamily: 'mk11', fontSize: 22 }}>weaknesses</Text>
+        </View>
+        <View>
+          {overviewObj.weaknesses.map((item, index) => {
+            return (
+              <View key={index}>
+                <Text style={[styles.arrayText, index % 2 === 0 ? styles.even : styles.odd]}>
+                  {item}
+                </Text>
+              </View>
+            );
+          })}
+        </View>
       </View>
     );
   };
 
   return (
-    <View style={styles.container}>
-      <RenderStrengths />
-      <RenderWeaknesses />
-      <Text style={styles.text}>{overviewObj.intro}</Text>
+    <View>
+      <Text style={styles.introText}>{overviewObj.intro}</Text>
+      <View style={styles.container}>
+        <RenderStrengths />
+        <RenderWeaknesses />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
-  text: {
+  container: {
+    borderWidth: 1,
+    borderColor: 'white',
+    marginTop: 15,
+  },
+  introText: {
     color: 'white',
+  },
+  arrayText: {
+    color: 'white',
+    fontSize: 18,
+    textAlign: 'center',
+    padding: 5,
+  },
+  even: {
+    backgroundColor: '#333232',
+  },
+  odd: {
+    backgroundColor: '#4a4949',
   },
 });
