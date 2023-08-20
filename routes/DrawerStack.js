48730b9f2bfb241aeac20462c966696c052be430
AlbanMarku <albanmarku@outlook.com>
2023-08-20 16:12:46 +0100

    First time fetch.

--- routes/DrawerStack.js
+++ routes/DrawerStack.js
@@ -1,16 +1,33 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { ToastAndroid } from 'react-native';
 import Home from '../views/Home';
 import About from '../views/About';
 import HeaderComp from '../components/HeaderComp';
 import { client, urlFor } from '../components/SanityClient';
 import { setupDb } from '../myDb';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Drawer = createDrawerNavigator();
 
 export default function DrawerStack() {
   const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    // Check if the app has been opened before
+    try {
+      AsyncStorage.getItem('appOpened').then((value) => {
+        if (!value) {
+          // This is the first time the app is opened
+          AsyncStorage.setItem('appOpened', 'true'); // Set the flag
+
+          fetchroster();
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
   const fetchRoster = async () => {
     console.log('fetching...');
@@ -18,7 +35,7 @@ export default function DrawerStack() {
     //Fetch all karacters. Get their name, avatar, profile image. Send the fetched roster to the database for offline access.
     try {
       const queryData = await client.fetch(
-        " *[_type == 'kharacter']{ _id,name, avatar, profile, basicAttacks[]{..., attackType->{name}},stringAttacks[]{..., attackType->{name}},basicAttacks[]{..., attackType->{name}},specialAttacks[]{...,attackType->{name}}, guide}"
+        "*[_type == 'kharacter']{ _id,name, avatar, profile, basicAttacks[]{..., attackType->{name}},stringAttacks[]{..., attackType->{name}},basicAttacks[]{..., attackType->{name}},specialAttacks[]{...,attackType->{name}}, guide}"
       );
       const mainData = queryData.map((item) => {
         const parsedAvatarImg = urlFor(item.avatar.asset._ref);
